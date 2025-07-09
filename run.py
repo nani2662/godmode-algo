079bb721a9eb64bbbd5ee0f9aaa9608fa59540fc
nani2662 <naveenkumar2662@gmail.com>
2025-07-09 10:15:48 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
 # Main script to start backend
+from backend.app import main
+
+if __name__ == "__main__":
+    main()
+
+    # Keep Render server alive
+    import time
+    while True:
+        time.sleep(60)
